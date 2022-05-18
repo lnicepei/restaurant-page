5a5e9b2b08f95663abbee087033b3674f5be1115
lnicepei <dmitrii.shaidorov@gmail.com>
2022-05-18 10:21:19 +0300

    Fix import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { createHomepage } from "./page1";
 import { createMenu } from "./page2";
-import { createHomePage2 } from ".page3/"
+import { createAboutPage } from "./page3";
+
+createHomepage();
 
 document.querySelector('#button1').addEventListener("click", createHomepage);
 document.querySelector('#button2').addEventListener("click", createMenu);
-document.querySelector('#button3').addEventListener("click", createHomePage2);
+document.querySelector('#button3').addEventListener("click", createAboutPage);
+
