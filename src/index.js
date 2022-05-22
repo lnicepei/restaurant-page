3d20259ae48e1d039c823f344fe7e8179a29287c
lnicepei <dmitrii.shaidorov@gmail.com>
2022-05-22 08:22:25 +0300

    Add 1 button

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import { createHomepage } from "./page1";
-import { createMenu } from "./page2";
-import { createAboutPage } from "./page3";
+import { createHomepage } from "./home";
+import { createMenu } from "./menu";
+import { createContactsPage } from "./contacts";
+import { createMap } from "./map";
 
 createHomepage();
 
 document.querySelector('#button1').addEventListener("click", createHomepage);
 document.querySelector('#button2').addEventListener("click", createMenu);
-document.querySelector('#button3').addEventListener("click", createAboutPage);
-
+document.querySelector('#button3').addEventListener("click", createContactsPage);
+document.querySelector('#button4').addEventListener("click", createMap);
