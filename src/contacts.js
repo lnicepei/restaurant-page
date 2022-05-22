4b14e06e0cd974bab77482d0bf68bf069f90d349
lnicepei <dmitrii.shaidorov@gmail.com>
2022-05-22 08:21:46 +0300

    Create contact page

---
+++ src/contacts.js
@@ -0,0 +1,28 @@
+function createContactsPage() {
+    const main = document.querySelector('.main');
+    main.innerHTML = "";
+
+    const info = document.createElement('div');
+    document.querySelector('.main').appendChild(info);
+    info.className = 'info';
+
+    const phone = document.createElement('div');
+    phone.textContent = "Phone: +12145237052";
+    
+
+    const address = document.createElement('div');
+    address.textContent = "Address: NYC";
+
+
+    const email = document.createElement('div');
+    email.textContent = "Email: veganfood@gmail.com";
+
+    info.appendChild(phone);
+    info.appendChild(address);
+    info.appendChild(email);
+}
+
+
+export {
+    createContactsPage 
+}
